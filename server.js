f82328a9a4748cdd7095f81f8e3d8f08047849de
Daniel Schechtman <daschechtman@gmail.com>
2020-12-16 16:49:02 -0600

    deploy to heroku

---
+++ server.js
@@ -0,0 +1,19 @@
+//Install express server
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const app_name = "PetShop"
+const path1 = `/dist/${app_name}`
+const path2 =`${path1}/index.html`
+
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + path1));
+
+app.get('/*', function(req,res) {
+    
+res.sendFile(path.join(__dirname + path2));
+});
+
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
